672a1a9b645bf57b09b02817641714078c1ed7e3
RyuNen344 <s1100633@outlook.com>
2019-06-14 14:31:46 +0900

    tweet詳細画面の追加

---
+++ app/src/main/java/com/ryunen344/kdroid/tweetDetail/TweetDetailPresenter.kt
@@ -0,0 +1,10 @@
+package com.ryunen344.kdroid.tweetDetail
+
+class TweetDetailPresenter : TweetDetailContract.Presenter {
+
+
+    override fun start() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
