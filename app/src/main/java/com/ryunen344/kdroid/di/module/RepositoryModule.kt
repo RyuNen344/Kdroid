6d90b2e1084b7dda2970d98989fe2be7303ea021
RyuNen344 <s1100633@outlook.com>
2019-07-31 00:09:57 +0900

    create repositoryImpl class

--- app/src/main/java/com/ryunen344/kdroid/di/module/RepositoryModule.kt
+++ app/src/main/java/com/ryunen344/kdroid/di/module/RepositoryModule.kt
@@ -1,8 +1,12 @@
 package com.ryunen344.kdroid.di.module
 
+import com.ryunen344.kdroid.domain.repository.AccountRepositoryImpl
+import com.ryunen344.kdroid.domain.repository.TwitterMediaRepositoryImpl
 import com.ryunen344.kdroid.domain.repository.TwitterRepositoryImpl
 import org.koin.dsl.module
 
 val RepositoryModule = module {
     single { TwitterRepositoryImpl() }
+    single { TwitterMediaRepositoryImpl() }
+    single { AccountRepositoryImpl() }
 }
---
+++ app/src/main/java/com/ryunen344/kdroid/domain/repository/AccountRepositoryImpl.kt
@@ -0,0 +1,42 @@
+package com.ryunen344.kdroid.domain.repository
+
+import com.ryunen344.kdroid.domain.database.AccountDatabase
+import com.ryunen344.kdroid.domain.entity.Account
+import com.ryunen344.kdroid.domain.entity.AccountAndAccountDetail
+import com.ryunen344.kdroid.domain.entity.AccountDetail
+import io.reactivex.Completable
+import io.reactivex.Flowable
+import io.reactivex.Single
+
+class AccountRepositoryImpl : AccountRepository {
+
+    private var accountDatabase : AccountDatabase? = AccountDatabase.getInstance()
+    private lateinit var accountDao : AccountRepository
+
+    init {
+        accountDatabase?.let { it ->
+            accountDao = it.accountRepository()
+        }
+    }
+
+    override fun findAccountList() : Single<List<AccountAndAccountDetail>> {
+        return accountDao.findAccountList()
+    }
+
+    override fun loadAccountById(id : Long) : Flowable<AccountAndAccountDetail> {
+        return accountDao.loadAccountById(id)
+    }
+
+    override fun insertAccount(account : Account) : Completable {
+        return accountDao.insertAccount(account)
+    }
+
+    override fun deleteAccount(account : Account) : Completable {
+        return accountDao.deleteAccount(account)
+    }
+
+    override fun insertAccountDetail(accountDetail : AccountDetail) : Completable {
+        return accountDao.insertAccountDetail(accountDetail)
+    }
+
+}
