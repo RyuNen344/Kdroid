9b730a9436db4b741077df2fcb9ba169a8753b97
RyuNen344 <s1100633@outlook.com>
2019-08-29 01:43:54 +0900

    add wallpaper to home activity

--- app/src/main/java/com/ryunen344/twikot/home/HomeActivity.kt
+++ app/src/main/java/com/ryunen344/twikot/home/HomeActivity.kt
@@ -1,11 +1,13 @@
 package com.ryunen344.twikot.home
 
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.ryunen344.twikot.R.layout.activity_home
 import com.ryunen344.twikot.R.string.consumer_key
 import com.ryunen344.twikot.R.string.consumer_secret_key
 import com.ryunen344.twikot.di.provider.AppProvider
+import com.ryunen344.twikot.domain.repository.WallpaperRepositoryImpl
 import com.ryunen344.twikot.util.LogUtil
 import com.ryunen344.twikot.util.replaceFragmentInActivity
 import kotlinx.android.synthetic.main.activity_home.*
@@ -16,6 +18,7 @@ class HomeActivity : AppCompatActivity() {
 
     private val appProvider : AppProvider by inject()
     private val homeFragment : HomeFragment by inject()
+    private val wallpaperRepositoryImpl : WallpaperRepositoryImpl by inject()
 
     companion object {
         const val INTENT_KEY_USER_ID : String = "key_user_id"
@@ -43,7 +46,15 @@ class HomeActivity : AppCompatActivity() {
                 ?: homeFragment.also {
                     replaceFragmentInActivity(supportFragmentManager, it, homeFrame.id)
                 }
+    }
 
+    override fun onResume() {
+        LogUtil.d()
+        super.onResume()
+        wallpaperRepositoryImpl.getUri()?.let {
+            coordinatorLayout.background = BitmapDrawable(resources, it)
+            coordinatorLayout.background.alpha = 80
+        }
     }
 
 }
