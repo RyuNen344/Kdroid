ab51649aad6dca849c44bc9b9b0039a0beac20ae
RyuNen344 <s1100633@outlook.com>
2019-11-16 23:02:13 +0900

    [remove]remove koincomponent from viewmodel

--- app/src/main/java/com/ryunen344/twikot/home/HomeViewModel.kt
+++ app/src/main/java/com/ryunen344/twikot/home/HomeViewModel.kt
@@ -1,9 +1,8 @@
 package com.ryunen344.twikot.home
 
 import androidx.lifecycle.ViewModel
-import org.koin.core.KoinComponent
 
-class HomeViewModel : ViewModel(), KoinComponent {
+class HomeViewModel : ViewModel() {
 
 
 }
